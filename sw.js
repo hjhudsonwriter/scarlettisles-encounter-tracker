4a229b9f79046c5f01a468bc83acf3cd16a86fda
hjhudsonwriter <hjhudson.writer@gmail.com>
2026-01-07 01:27:39 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
 // Simple offline cache for GitHub Pages static hosting
 const CACHE_NAME = "encounter-tracker-cache-v2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest"
-];
-
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./sw.js", "./logo.png"];
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
